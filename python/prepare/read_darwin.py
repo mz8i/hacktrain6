d36980c5028d194f9c05f2baf0cab0c6bc0bfb34
Maciek <maciej.ziarkowski@gmail.com>
2019-11-16 17:40:37 +0000

    Pull out parsing of XML feed

--- python/prepare/read_darwin.py
+++ python/prepare/read_darwin.py
@@ -4,7 +4,10 @@ from .schema_v16 import parseString
 
 xml_header = '<?xml version="1.0" encoding="UTF-8"?>'
 
+def read_xml_feed(path: Path, header=xml_header, leave_header=False):
+    return [f"{header}\n{xml}" if leave_header else xml for xml in path.read_text().split(header)[1:]]
+
 def read_darwin(path: Path):
-    xmls = path.read_text().split(xml_header)[1:]
+    xmls = read_xml_feed(path)
     pports = [parseString(xml, silence=True) for xml in xmls]
     return pports
